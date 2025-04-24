b86a747484c7b47ab9e18cf7fde8de72f0a1f642
Felipe Pêpe <felipepepe21@gmail.com>
2025-04-24 12:37:48 -0300

    feat: :sparkles: updated section button

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const config = {
+  theme: {
+    extend: {
+      fontSize: {
+        xs: ["0.75rem", { lineHeight: "1rem" }], // 12px
+        sm: ["0.875rem", { lineHeight: "1.25rem" }], // 14px
+        md: ["1rem", { lineHeight: "1.5rem" }], // 16px
+        lg: ["1.25rem", { lineHeight: "1.75rem" }], // 20px
+        xl: ["2rem", { lineHeight: "2.25rem" }], // 32px
+        "2xl": ["2.5rem", { lineHeight: "2.75rem" }], // 40px
+      },
+    },
+  },
+};
+
+export default config;
